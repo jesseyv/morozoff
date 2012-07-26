c8603bb27fcdd9c0025c27417a6ca02d54d40ef2
Алексей <alexei.iljin@gmail.com>
2012-07-26 16:50:45 +0400

    intermediate

--- apps/customshop/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.forms import ModelForm
-from shop.models import OrderExtraInfo
-
-
-class OrderExtraInfoForm(ModelForm):
-    class Meta:
-        model = OrderExtraInfo
-        fields = ('text',)
