f79838ede6f1d1e9df5372e0651fbd00ca1542d1
Алексей <alexei.iljin@gmail.com>
2012-07-27 17:48:00 +0400

    nginx fixed

--- fabfile.py
+++ fabfile.py
@@ -2,50 +2,75 @@
 from fabric.api import *
 from fabric.colors import *
 
-packages = [
-    'lsb',
-    'python-pip',
-    'build-essential',
-    'libxml2-dev',
-    'puppet-common',
-]
+PROJECTS_ROOT = '/var/www/projects/'
 
+PROJECT_REPO_SSH_URL = local('git config --get remote.origin.url', capture=True)
+PROJECT_REPO_GIT_URL = 'git://{0}'.format(PROJECT_REPO_SSH_URL.split('@')[1].replace(':', '/'))
+PROJECT_REPO_NAME = local('basename {0} .git'.format(PROJECT_REPO_SSH_URL), capture=True)
 
-def _copy_ssh_keys():
-    print(yellow('Trying to copy SSH keys')) 
-    local('ssh-copy-id {0.user}@{0.host_string}'.format(env))
-    print(green('SSH keys copied')) 
+DEPLOY_USER = 'www-data'
 
-def _update_packages(upgrade=False):
-    print(green('Updating packages'))
-    run('apt-get update')
-    if upgrade:
-        print(green('Upgrading packages'))
-        run('apt-get upgrade -y')
+PACKAGES = ['libmemcached-dev']
 
-def _install_packages():
-    print(green('Installing packages'))
-    run('apt-get -yq install {0}'.format(' '.join(packages)))
-    
+def _sudo(command):
+    env.sudo_prefix = "sudo -S -i -p '%(sudo_prompt)s'"
+    return sudo(command, user=DEPLOY_USER)
 
-def setup_new():
-    _copy_ssh_keys()
-    with cd('/tmp'):
-        run('wget http://apt.puppetlabs.com/puppetlabs-release_1.0-3_all.deb')
-        run('dpkg -i puppetlabs-release_1.0-3_all.deb')
-    #_update_packages(True)
-    _install_packages()
-    put('./configs/puppet_default', '/etc/default/puppet')
-    #with settings(warn_only=True):
-    #    result = run('service puppet restart')
-    #    if result.failed:
-    #        run('service puppet start')
-        
-    print(green('Server ready for running Puppet manifests'))
-    
+def enable_proj():
+    print(green('Enabling project serving'))
+    run('ln -s -f {0}{1}/uwsgi.ini /etc/uwsgi/apps-enabled/{1}.ini'.format(PROJECTS_ROOT, PROJECT_REPO_NAME))
+    run('ln -s -f {0}{1}/nginx.conf /etc/nginx/sites-available/{1}'.format(PROJECTS_ROOT, PROJECT_REPO_NAME))
+    run('ln -s -f /etc/nginx/sites-available/{0} /etc/nginx/sites-enabled/{0}'.format(PROJECT_REPO_NAME))
+    restart_proj()
+
+def disable_proj():
+    print(green('Disabling project serving'))
+    run('rm /etc/uwsgi/apps-enabled/{0}.ini'.format(PROJECT_REPO_NAME))
+    run('rm /etc/nginx/sites-enabled/{0}'.format(PROJECT_REPO_NAME))
+
+def restart_proj():
     with settings(warn_only=True):
-        run('mkdir -p /etc/puppet/modules')
-    print(green('Uploading bootstrap modile to server'))
-    put('./puppet_modules/uwsgi_node', '/etc/puppet/modules')
-    print(green('Apply Puppet module'))
-    run('puppet apply /etc/puppet/modules/uwsgi_node/manifests/init.pp')
+        print(green('Restarting project serving'))
+        run('touch /etc/uwsgi/apps-enabled/{}.ini'.format(PROJECT_REPO_NAME))
+        run('service nginx reload')
+
+def deploy_init():
+    if PACKAGES:
+        print(green('Installing dependencies'))
+        run('apt-get -yq --force-yes install {0}'.format(' '.join(PACKAGES)))
+
+    with settings(warn_only=True):
+        with cd(PROJECTS_ROOT):
+            if run('test -d {0}'.format(PROJECT_REPO_NAME)).failed:
+                print(green('Cloning project\'s repo'))
+                _sudo('git clone {0}'.format(PROJECT_REPO_GIT_URL))
+
+            with cd(PROJECT_REPO_NAME):
+                print(green('Updating project\'s repo'))
+                _sudo('git reset --hard HEAD; git pull')
+                put('{0}/local_settings.py'.format(PROJECT_REPO_NAME), '{0}{1}'.format(PROJECTS_ROOT, PROJECT_REPO_NAME))
+                run('chown {0}:{0} local_settings.py'.format(DEPLOY_USER))
+                #put('fixtures', '{0}{1}'.format(PROJECTS_ROOT, PROJECT_REPO_NAME))
+                #run('chown -R {0}:{0} fixtures'.format(DEPLOY_USER))
+                WORK_HOME = _sudo('echo $WORKON_HOME')
+
+                if run('test -d {0}/{1}'.format(WORK_HOME, PROJECT_REPO_NAME)).failed:
+                    print(green('Creating virtualenv for project'))
+                    _sudo('mkvirtualenv {}'.format(PROJECT_REPO_NAME))
+
+                with prefix('workon {}'.format(PROJECT_REPO_NAME)):
+                    print(green('Installing required packets'))
+                    _sudo('pip install -r requirements.txt')
+
+                with prefix('workon {}'.format(PROJECT_REPO_NAME)):
+                    with cd('{0}'.format(PROJECT_REPO_NAME)):
+                        print(green('Syncing DB'))
+                        _sudo('python manage.py syncdb --noinput')
+
+                        print(green('Collecting static'))
+                        _sudo('python manage.py collectstatic --noinput')
+
+                        #print(green('Installing fixtures'))
+                        #_sudo('python manage.py loaddata fixtures/*.json')
+
+    enable_proj()
